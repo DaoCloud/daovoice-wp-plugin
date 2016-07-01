3439df7d0eaee81de18337ab8c465731cb533fde
叶挺 <me@yeting.info>
2016-07-01 10:28:00 +0800

    对接 WP 用户系统，获取用户信息传入 DaoVoice

--- daovoice_plugin.php
+++ daovoice_plugin.php
@@ -8,16 +8,35 @@ Author URI: http://www.daocloud.io/cloud/voice.html
 Version: 0.1
  */
 function daovoice_js(){
+	$wp_user = wp_get_current_user()
 	$app_id = getenv("DAOVOICE_APP_ID");
-	$str = <<<EOT
+
+	if(empty($app_id)){
+		return '';
+	}
+
+	$daovoiceSettingJson = [
+		"app_id" => $app_id
+	];
+
+	if(!empty($wp_user)) {
+		if (!empty($wp_user->user_email))
+		{
+		  $daovoiceSettingJson["email"] = WordPressEscaper::escJS($wp_user->user_email);
+		}
+		if (!empty($wp_user->display_name))
+		{
+		  $daovoiceSettingJson["name"] = WordPressEscaper::escJS($wp_user->display_name);
+		}
+	}
+
+	$str = <<<HTML
 <script>(function(i,s,o,g,r,a,m){i["DaoVoiceObject"]=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,"script","//widget.daovoice.io/widget/{$app_id}.js","daovoice");</script>
 <script>
-  daovoice('init', {
-    app_id: "$app_id",
-  });
+  daovoice('init', $daovoiceSettingJson);
   daovoice('update');
 </script>
-EOT;
+HTML;
 	echo $str;
 }
 
